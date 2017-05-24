1e74102c538b76d2d94af0ea9933e9d4596768a3
localadmin <localadmin@roberts-air-3.zeevee.com>
2017-05-24 11:20:59 -0400

    my change

--- app/src/component/zone/ZoneHome.js
+++ app/src/component/zone/ZoneHome.js
@@ -152,6 +152,7 @@ class ZoneHome extends Component {
               onChange={ size => localStorage.setItem('zoneTreeview', size) }>
               <div style={{ ...compStyle, marginTop: 0 }}>
                 {/* <ZoneTreeview /> */}
+                <div>hey</div>
                 <Calendar />
               </div>
               <div style={{ ...compStyle, marginLeft: 10, marginTop: 0 }}>
